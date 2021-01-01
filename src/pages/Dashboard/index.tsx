ed70f778ba563a78d1d530bea1cad31a50e1bb4d
Tiago Rodrigues <tiagoadriano.rodrigues@gmail.com>
2021-01-01 14:03:25 -0300

    Add dashboard styles and Github API

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,71 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useState, FormEvent } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+
+import api from '../../services/api';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/images/github-explorer.svg';
+
+interface Repository {
+  id: number;
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  const [newRepository, setNewRepository] = useState(
+    'tiagords/gostack-conceitos-nodejs',
+  );
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepository}`);
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepository('');
+  }
+
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form onSubmit={handleAddRepository}>
+        <input
+          type="text"
+          placeholder="Digite o nome do repósitorio"
+          value={newRepository}
+          onChange={e => setNewRepository(e.target.value)}
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        {repositories.map(repository => (
+          <a key={repository.id} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -29,5 +29,4 @@ body, input, button{
 button {
   cursor: pointer;
 }
-
 `;
